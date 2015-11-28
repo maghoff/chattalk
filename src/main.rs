96c070aecb3c83b17003ff7f871b59178eef8179
Magnus Hoff <maghoff@gmail.com>
2015-11-28 13:35:15 +0100

    PlainTalk echo server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 extern crate plaintalk;
 
 use std::convert;
-use std::io::{self,Read,Write};
+use std::io::{self,Read,Write,BufReader,BufWriter};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 use plaintalk::pullparser::PullParser;
+use plaintalk::pushgenerator::PushGenerator;
 
 #[derive(Debug)]
 enum ClientError {
 	Io(io::Error),
 	St(&'static str),
+	PushGenerator(plaintalk::pushgenerator::Error),
 }
 
 impl convert::From<io::Error> for ClientError {
@@ -24,14 +26,25 @@ impl convert::From<&'static str> for ClientError {
 	}
 }
 
-fn client_core(mut stream: TcpStream) -> Result<(), ClientError> {
-	let mut parser = PullParser::new(&mut stream);
+impl convert::From<plaintalk::pushgenerator::Error> for ClientError {
+	fn from(err: plaintalk::pushgenerator::Error) -> ClientError {
+		ClientError::PushGenerator(err)
+	}
+}
+
+fn client_core(stream: TcpStream) -> Result<(), ClientError> {
+	let mut buf_reader = BufReader::new(&stream);
+	let mut parser = PullParser::new(&mut buf_reader);
+
+	let mut buf_writer = BufWriter::new(&stream);
+	let mut generator = PushGenerator::new(&mut buf_writer);
 
 	while let Some(mut message) = try!{parser.get_message()} {
+		let mut reply = try!{generator.next_message()};
 		while let Some(mut field) = try!{message.get_field()} {
 			let mut buffer = String::new();
 			try!{field.read_to_string(&mut buffer)};
-			println!("Field: {}", &buffer);
+			try!{try!{reply.next_field()}.write_all(&buffer.into_bytes())};
 		}
 	}
 
