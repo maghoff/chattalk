78479083da7ab415eb0d72d65c647a78ba3e4699
Magnus Hoff <maghoff@gmail.com>
2015-12-18 22:19:26 +0100

    Add support for accepting clients via Unix Domain Sockets

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 extern crate plaintalk;
 extern crate crossbeam;
+extern crate unix_socket;
 
 mod client;
 
-use std::io::{BufReader,BufWriter};
-use std::net::{TcpListener,TcpStream};
-use std::thread;
-use std::sync::{Arc,Mutex};
+use std::io::{BufReader,BufWriter,Read,Write};
+use std::net::TcpListener;
 use std::sync::mpsc::{channel,Sender,Receiver};
+use std::sync::{Arc,Mutex};
+use std::thread;
 use plaintalk::pullparser::PullParser;
 use plaintalk::pushgenerator::PushGenerator;
+use unix_socket::UnixListener;
 
-fn client_core(stream: TcpStream, tx: Sender<ShoutMessage>) -> Result<(), client::ClientError> {
-	let mut buf_reader = BufReader::new(&stream);
+fn client_core<R: Read, W: Write+Send>(read: R, write: W, tx: Sender<ShoutMessage>) -> Result<(), client::ClientError> {
+	let mut buf_reader = BufReader::new(read);
 	let parser = PullParser::new(&mut buf_reader);
 
-	let buf_writer = BufWriter::new(&stream);
+	let buf_writer = BufWriter::new(write);
 	let generator = Arc::new(Mutex::new(PushGenerator::new(buf_writer)));
 
 	let (tx2, rx2) = channel::<ClientMessage>();
@@ -43,11 +45,11 @@ fn client_core(stream: TcpStream, tx: Sender<ShoutMessage>) -> Result<(), client
 	//tx.send(ShoutMessage::Part(tx2)).unwrap();
 }
 
-fn handle_client(stream: TcpStream, tx: Sender<ShoutMessage>) {
-	let remote = stream.peer_addr().unwrap();
+fn handle_client<R: Read, W: Write+Send>(read: R, write: W, remote: &str, tx: Sender<ShoutMessage>) {
+	//let remote = stream.peer_addr().unwrap();
 	println!("{} Client connected", remote);
 
-	match client_core(stream, tx) {
+	match client_core(read, write, tx) {
 		Ok(()) => println!("{} Connection closed", remote),
 		Err(e) => println!("{} Connection terminated with error: {:?}", remote, e),
 	}
@@ -85,7 +87,7 @@ fn server(rx : Receiver<ShoutMessage>) {
 	}
 }
 
-fn network_reader(tx : Sender<ShoutMessage>) {
+fn tcp_acceptor(tx : Sender<ShoutMessage>) {
 	let listener = TcpListener::bind("127.0.0.1:2203").unwrap();
 	println!("Listening to {}", listener.local_addr().unwrap());
 
@@ -93,7 +95,30 @@ fn network_reader(tx : Sender<ShoutMessage>) {
 		match stream {
 			Ok(stream) => {
 				let tx = tx.clone();
-				thread::spawn(move || { handle_client(stream, tx) });
+				thread::spawn(move || {
+					let remote = format!("{}", stream.peer_addr().unwrap());
+					handle_client(&stream, &stream, &remote, tx)
+				});
+			}
+			Err(e) => {
+				println!("Failed connection attempt: {:?}", e);
+			}
+		}
+	}
+}
+
+fn unix_acceptor(tx : Sender<ShoutMessage>) {
+	let listener = UnixListener::bind("socket").unwrap();
+	println!("Listening to {:?}", listener.local_addr().unwrap());
+
+	for stream in listener.incoming() {
+		match stream {
+			Ok(stream) => {
+				let tx = tx.clone();
+				thread::spawn(move || {
+					let remote = format!("{:?}", stream.peer_addr().unwrap());
+					handle_client(&stream, &stream, &remote, tx)
+				});
 			}
 			Err(e) => {
 				println!("Failed connection attempt: {:?}", e);
@@ -104,6 +129,10 @@ fn network_reader(tx : Sender<ShoutMessage>) {
 
 fn main() {
 	let (tx, rx) = channel();
-	thread::spawn(move || network_reader(tx));
+	{
+		let tx = tx.clone();
+		thread::spawn(move || tcp_acceptor(tx));
+	}
+	thread::spawn(move || unix_acceptor(tx));
 	server(rx);
 }
